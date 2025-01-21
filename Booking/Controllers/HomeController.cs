8aec49fc53ba6c742f43a62c7b0287f0af2d4ed5
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-01-21 20:49:29 +0700

    Update appsettings.json

--- Booking/Controllers/HomeController.cs
+++ Booking/Controllers/HomeController.cs
@@ -1,32 +1,368 @@
-using Booking.Models;
+﻿using Booking.Models;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
+using Booking.ViewModels;
+using Booking.Services;
 
 namespace Booking.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IEmailSender _emailSender;
+
+        public HomeController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, IEmailSender emailSender = null)
         {
-            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _emailSender = emailSender;
         }
 
-        public IActionResult Index()
+        //  private readonly IEmailSender _emailSender;
+
+        public async Task<IActionResult> Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Login(string ReturnUrl = null)
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (!string.IsNullOrWhiteSpace(ReturnUrl) && Url.IsLocalUrl(ReturnUrl) && ReturnUrl != Url.Action("Login", "Home")
+                && ReturnUrl != Url.Action("Register", "Home") && ReturnUrl != Url.Action("Forgot", "Home")
+                && ReturnUrl != Url.Action("ResetPassword", "Home")
+                )
+            {
+                ViewData["ReturnUrl"] = ReturnUrl;
+            }
+            else
+            {
+                ViewData["ReturnUrl"] = "/";
+            }
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string username, string password,bool? rememberMe, string ReturnUrl = null)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                return Json(new { status = "error", msg = "Tên Người Dùng Không Được Để Trống" });
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                return Json(new { status = "error", msg = "Mật Khẩu Không Được Để Trống" });
+            }
+            // Xử lý ReturnUrl tương tự GET
+            if (!string.IsNullOrWhiteSpace(ReturnUrl) && Url.IsLocalUrl(ReturnUrl) && ReturnUrl != Url.Action("Login", "Home")
+                && ReturnUrl != Url.Action("Register", "Home") && ReturnUrl != Url.Action("Forgot", "Home")
+                && ReturnUrl != Url.Action("ResetPassword", "Home")
+                )
+            {
+                ViewData["ReturnUrl"] = ReturnUrl;
+            }
+            else
+            {
+                ViewData["ReturnUrl"] = "/";
+            }
+            var user = await _userManager.FindByNameAsync(username) ?? await _userManager.FindByEmailAsync(username);
+            if (user == null)
+            {
+                return Json(new { status = "error", msg = "Tài khoản không tồn tại" });
+            }
+           /* if (user.IsLockedByAdmin)
+            {
+                return Json(new { status = "error", msg = "Tài khoản của bạn đã bị khóa bởi quản trị viên." });
+            }*/
+            if (!await _userManager.IsEmailConfirmedAsync(user))
+            {
+                return Json(new { status = "comfirm", msg = "Bạn phải xác thực email trước khi đăng nhập." });
+            }
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, password);
+            if (!isPasswordValid)
+            {
+                await _userManager.AccessFailedAsync(user);
+                var failedAttempts = await _userManager.GetAccessFailedCountAsync(user);
+
+                if (await _userManager.IsLockedOutAsync(user))
+                {
+                    return Json(new { status = "error", msg = "Tài khoản của bạn đã bị khóa do quá nhiều lần đăng nhập thất bại." });
+                }
+                else
+                {
+                    return Json(new { status = "error", msg = $"Sai mật khẩu! Bạn còn {5 - failedAttempts} lần thử." });
+                }
+            }
+
+            await _userManager.ResetAccessFailedCountAsync(user);
+
+            var result = await _signInManager.PasswordSignInAsync(user, password, true, lockoutOnFailure: true);
+            if (result.IsLockedOut)
+            {
+                return Json(new { status = "error", msg = "Tài khoản của bạn đã bị khóa." });
+            }
+            var isTwoFactorEnabled = await _userManager.GetTwoFactorEnabledAsync(user);
+            if (isTwoFactorEnabled)
+            {
+                var token = await _userManager.GenerateTwoFactorTokenAsync(user, "Authenticator");
+                return Json(new { status = "verify", msg = "Nhập mã xác minh từ ứng dụng xác thực.", token = token });
+            }
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: rememberMe ?? false); // Thêm dòng này
+                return Json(new
+                    {
+                        status = "success",
+                        msg = "Đăng nhập thành công",
+                        redirectUrl = ViewData["ReturnUrl"]?.ToString()
+                    });
+     
+            }
+            return Json(new { status = "error", msg = "Thông tin đăng nhập không chính xác!" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResendConfirmationEmail(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username) ?? await _userManager.FindByEmailAsync(username);
+            if (user == null)
+            {
+                return Json(new { status = "error", msg = "Tài khoản không tồn tại" });
+            }
+
+            // Generate a new email confirmation token
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            // Create the confirmation link
+            var confirmationLink = Url.Action("ConfirmEmail", "Home", new { userId = user.Id, token = token }, Request.Scheme);
+
+            // Send the confirmation email
+            await _emailSender.SendEmailAsync(user.Email, "Email Verification",
+                $"Please click the following link to verify your email: {confirmationLink}");
+
+            return Ok(new { message = "A new verification email has been sent." });
+        }
+
+        [AllowAnonymous]
+        public IActionResult LoginWithProdider(string provider)
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Home");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+
+            // Xử lý chọn provider (Google hoặc Microsoft)
+            if (provider == "Google")
+            {
+                return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+            }
+            else if (provider == "Microsoft")
+            {
+                return Challenge(properties, MicrosoftAccountDefaults.AuthenticationScheme);
+            }
+
+            // Nếu không xác định provider, trả về trang đăng nhập mặc định
+            return RedirectToAction("Login");
+        }
+        [AllowAnonymous]
+        public async Task<IActionResult> ExternalLoginCallback()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            if (!result.Succeeded)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var claims = result.Principal.Identities.FirstOrDefault()?.Claims;
+            var email = claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+
+            // Kiểm tra nếu user đã tồn tại trong hệ thống
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    UserName = email,
+                    Email = email
+                };
+
+                var createUserResult = await _userManager.CreateAsync(user);
+
+                if (!createUserResult.Succeeded)
+                {
+                    ModelState.AddModelError(string.Empty, "Error creating user.");
+                    return RedirectToAction("Login");
+                }
+
+                if (!await _roleManager.RoleExistsAsync("user"))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole("user"));
+                }
+
+                await _userManager.AddToRoleAsync(user, "user");
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: true);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [HttpGet]
+        public IActionResult Register(string ReturnUrl = null)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var userName = User.Identity.Name;
+                return RedirectToAction("Index", "Home");
+            }
+            // Xử lý ReturnUrl tương tự GET
+            if (!string.IsNullOrWhiteSpace(ReturnUrl) && Url.IsLocalUrl(ReturnUrl) && ReturnUrl != Url.Action("Login", "Home")
+                && ReturnUrl != Url.Action("Register", "Home") && ReturnUrl != Url.Action("Forgot", "Home")
+                && ReturnUrl != Url.Action("ResetPassword", "Home")
+                )
+            {
+                ViewData["ReturnUrl"] = ReturnUrl;
+            }
+            else
+            {
+                ViewData["ReturnUrl"] = "/";
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                if (ModelState.ContainsKey("Username") && ModelState["Username"].Errors.Any())
+                {
+                    return Json(new { status = "error", msg = "" + ModelState["Username"].Errors[0].ErrorMessage });
+                }
+
+                if (ModelState.ContainsKey("Email") && ModelState["Email"].Errors.Any())
+                {
+                    return Json(new { status = "error", msg = "" + ModelState["Email"].Errors[0].ErrorMessage });
+                }
+
+                if (ModelState.ContainsKey("Password") && ModelState["Password"].Errors.Any())
+                {
+                    return Json(new { status = "error", msg = "" + ModelState["Password"].Errors[0].ErrorMessage });
+                }
+
+                if (ModelState.ContainsKey("repassword") && ModelState["repassword"].Errors.Any())
+                {
+                    return Json(new { status = "error", msg = "" + ModelState["repassword"].Errors[0].ErrorMessage });
+                }
+
+                return Json(new { status = "error", msg = "Dữ liệu không hợp lệ" });
+            }
+
+            if (!string.IsNullOrWhiteSpace(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl) && model.ReturnUrl != Url.Action("Login", "Home")
+                && model.ReturnUrl != Url.Action("Register", "Home") && model.ReturnUrl != Url.Action("Forgot", "Home")
+                && model.ReturnUrl != Url.Action("ResetPassword", "Home")
+                )
+            {
+                ViewData["ReturnUrl"] = model.ReturnUrl;
+            }
+            else
+            {
+                ViewData["ReturnUrl"] = "/";
+            }
+            var existingUser = await _userManager.FindByNameAsync(model.Username);
+            if (existingUser != null)
+            {
+                return Json(new { status = "error", msg = "Tên người dùng đã tồn tại. Vui lòng chọn tên khác." });
+            }
+            var existingEmail = await _userManager.FindByEmailAsync(model.Email);
+            if (existingEmail != null)
+            {
+                return Json(new { status = "error", msg = "Email đã được đăng ký. Vui lòng sử dụng email khác." });
+            }
+            var user = new AppUser
+            {
+                UserName = model.Username,
+                Email = model.Email,
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                 
+                var confirmationLink = Url.Action("ConfirmEmail", "Home",
+                    new { userId = user.Id, token = token }, Request.Scheme);
+
+                // Gửi email xác minh
+                await _emailSender.SendEmailAsync(user.Email, "Xác nhận email",
+                    $"Vui lòng nhấp vào liên kết để xác nhận email: {confirmationLink}");
+                await _signInManager.SignInAsync(user, isPersistent: true);
+
+
+                return Json(new { status = "success", msg = "Đăng ký thành công.", redirectUrl = ViewData["ReturnUrl"].ToString() });
+
+            }
+            var firstResultError = result.Errors.FirstOrDefault()?.Description;
+            return Json(new { status = "error", msg = firstResultError ?? "Đăng ký thất bại." });
+        }
+
+        // Xác nhận email khi người dùng nhấp vào liên kết trong email
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            if (userId == null || token == null)
+            {
+                return BadRequest("Yêu cầu không hợp lệ.");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound("Không tìm thấy người dùng.");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            if (result.Succeeded)
+            {
+                return Ok("Xác nhận email thành công.");
+            }
+
+            return BadRequest("Xác nhận email không thành công.");
+        }
+
+        public async Task<IActionResult> LogoutAsync()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+               /* var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var getUser = await this._userManager.FindByIdAsync(userId);
+                getUser.lastAssces = DateTime.Now;
+                await this._userManager.UpdateAsync(getUser);*/
+            }
+            await _signInManager.SignOutAsync();
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
--- Booking/Program.cs
+++ Booking/Program.cs
@@ -1,8 +1,11 @@
-using Booking.Data;
+﻿using Booking.Data;
 using Booking.Models;
 using Booking.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using static Booking.Services.EmailService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +19,131 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddIdentity<AppUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>().AddErrorDescriber<VietnameseIdentityErrorDescriber>().AddDefaultTokenProviders(); ;
 builder.Services.AddControllersWithViews();
-builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddRazorPages();
+
+var mailsettings = builder.Configuration.GetSection("MailSettings");
+builder.Services.Configure<MailSettings>(mailsettings);
+builder.Services.AddTransient<IEmailSender, SendMailService>();
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Home/Login"; // Đường dẫn trang đăng nhập
+        options.AccessDeniedPath = "/Home/AccessDenied"; // Đường dẫn khi bị từ chối truy cập
+        options.LoginPath = "/Home/Login"; // Đường dẫn đến trang đăng nhập
+        options.LogoutPath = "/Home/Logout"; // Đường dẫn đến trang đăng xuất
+        options.AccessDeniedPath = "/Account/AccessDenied"; // Đường dẫn khi truy cập bị từ chối
+        options.ReturnUrlParameter = "ReturnUrl"; // Xác định tham số query string
+        options.ExpireTimeSpan = TimeSpan.FromDays(14); // Thời gian lưu cookie
+        options.SlidingExpiration = true; // Làm mới thời gian hết hạn cookie
+    })
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme; // Lưu thông tin đăng nhập qua Cookies
+    options.CallbackPath = "/signin-google";
+
+    // Hiển thị trang chọn tài khoản
+    options.Events.OnRedirectToAuthorizationEndpoint = context =>
+    {
+        context.Response.Redirect(context.RedirectUri + "&prompt=select_account");
+        return Task.CompletedTask;
+    };
+
+    // Xử lý lỗi khi xác thực
+    options.Events.OnRemoteFailure = u =>
+    {
+        u.Response.Redirect("/Home/Login");
+        u.HandleResponse();
+        return Task.CompletedTask;
+    };
+}).AddMicrosoftAccount(microsoftOptions =>
+{
+    microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+    microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+    microsoftOptions.CallbackPath = builder.Configuration["Authentication:Microsoft:CallbackPath"];
+    microsoftOptions.Scope.Add("email");
+    microsoftOptions.Scope.Add("profile");
+    microsoftOptions.Scope.Add("openid");
+    microsoftOptions.SaveTokens = true;
+});
+
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Thời gian hết hạn session
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    // Chính sách cho Admin
+    options.AddPolicy("AdminPolicy", policy =>
+        policy.RequireRole("Admin"));
+
+    // Chính sách cho CTV
+    options.AddPolicy("CTVPolicy", policy =>
+        policy.RequireRole("Admin", "Seller"));
+
+});
+
+
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequiredUniqueChars = 1;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+
+    //setting for user
+    options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;
+
+    options.SignIn.RequireConfirmedAccount = true;
+    options.SignIn.RequireConfirmedEmail = true;
+
+});
+// Cấu hình Cookie
+/*builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Home/Login"; // Đường dẫn đến trang đăng nhập
+    options.LogoutPath = "/Home/Logout"; // Đường dẫn đến trang đăng xuất
+    options.AccessDeniedPath = "/Account/AccessDenied"; // Đường dẫn khi truy cập bị từ chối
+    options.ReturnUrlParameter = "ReturnUrl"; // Xác định tham số query string
+    options.ExpireTimeSpan = TimeSpan.FromDays(14); // Thời gian lưu cookie
+    options.SlidingExpiration = true; // Làm mới thời gian hết hạn cookie
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
@@ -38,7 +164,14 @@ app.UseStaticFiles();
 app.MapRazorPages();
 app.UseRouting();
 
+
+await SeedDataAsync(app);
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseSession();
+
+
 
 app.MapControllerRoute(
     name: "default",
@@ -46,3 +179,61 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+static async Task SeedDataAsync(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = services.GetRequiredService<UserManager<AppUser>>();
+
+        // Create roles if they don't exist
+        string[] roles = { "User", "Admin", "Seller" };
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+
+
+        // Create a default User user if it doesn't exist
+        var User = await userManager.FindByEmailAsync("thanhpqce171732@fpt.edu.vn");
+        if (User == null)
+        {
+           User = new AppUser { UserName = "thanhmax14", Email = "thanhpqce171732@fpt.edu.vn" };
+            var result = await userManager.CreateAsync(User, "Password123!");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(User, "User");
+            }
+        }
+
+
+        
+        // Create a default admin user if it doesn't exist
+        var adminUser = await userManager.FindByEmailAsync("admin@gmail.com");
+        if (adminUser == null)
+        {
+            adminUser = new AppUser { UserName = "admin", Email = "admin@gmail.com" };
+            var result = await userManager.CreateAsync(adminUser, "Password123!");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+
+        // Create a default CTV user if it doesn't exist
+        var ctvUser = await userManager.FindByEmailAsync("ctv@gmail.com");
+        if (ctvUser == null)
+        {
+            ctvUser = new AppUser { UserName = "ctv", Email = "ctv@gmail.com" };
+            var result = await userManager.CreateAsync(ctvUser, "Password123!");
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(ctvUser, "Seller");
+            }
+        }
+    }
+}
