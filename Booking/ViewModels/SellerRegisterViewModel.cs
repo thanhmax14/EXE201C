176a42356bd693cededd7e89709063956041d326
Gabi9991 <khoinguyen101103@gmail.com>
2025-02-07 16:48:38 +0700

    Admin function

---
+++ Booking/ViewModels/SellerRegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booking.ViewModels
+{
+    public class SellerRegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, Phone]
+        public string Phone { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public string District { get; set; }
+        [Required]
+        public string Ward { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public bool AcceptTerms { get; set; }
+    }
+}
