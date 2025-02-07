89f459989e07d78d0c00c7fc266812b4c8d592b3
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-02-07 21:11:27 +0700

    as

---
+++ Booking/ViewModels/ManaHotelBoking.cs
@@ -0,0 +1,21 @@
+﻿namespace Booking.ViewModels
+{
+    public class ManaHotelBoking
+    {
+        public string orderCode { get; set; }
+        public string? hotemName { get; set; }
+        public string? addreddHotel { get; set; }
+        public Guid HotelID { get; set; }
+        public string? roomNAME { get; set; }
+        public string? guest { get; set; }
+        public string? user { get; set; }
+        public string? addresUser { get; set; }
+        public string?  date { get; set; }
+        public decimal? price { get; set; } = 0;
+        public string?  booked { get; set; }
+        public string? status { get; set; }
+        public Guid DatphongID { get; set; }
+
+
+    }
+}
---
+++ Booking/ViewModels/viewModelDash.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.ViewModels
+{
+    public class viewModelDash
+    {
+        public int totelBoking { get; set; } = 0;
+        public int totekList { get; set; } = 0;
+        public int totelReviw { get; set; } = 0;
+    }
+}
